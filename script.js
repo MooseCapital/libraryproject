3f36b9d29ce8d3f0e167488236ed207317f069f6
MooseCapital <48196495+MooseCapital@users.noreply.github.com>
2022-06-06 22:03:33 -0500

    book form added
    Former-commit-id: 0e220e2c5762cf61bb505ccb4bf5ef57470466b0

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let booktitle = document.querySelector(".booktitle");
+let bookauthor = document.querySelector(".bookauthor");
+let bookpages = document.querySelector(".bookpages");
+let bookread= document.querySelector(".bookread");
+const addButton = document.querySelector(".addto");
+let maincontent = document.querySelector(".maincontent");
+
+
+
+
+
+addButton.addEventListener("click", createClone);
 
 
 function Book(title, author, pages, read, cover) {
@@ -5,34 +17,85 @@ function Book(title, author, pages, read, cover) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.image = cover;
+    
 }
 
 Book.prototype.info = function () {
     return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read} `;
 }
 //"The" + title + "by" +  author
-const Hobbit = new Book("Hobbit","J.R.R Tolkien", 295, true, "images/hobbitcover.jpeg");
+const Hobbit = new Book("The Hobbit","J.R.R Tolkien", 295, false);
 console.log(Hobbit.info())
 
 
 
-let myLibrary = [];
-myLibrary.push(Hobbit)
-console.table(myLibrary)
+let Library = [];
+
+console.table(Library)
 function addBooktoLibrary() {
 
 
 }
+function addExampleBooks(bookobj, index) {
+    Library.push(bookobj)
+    booktitle.innerText = Library[index].title;
+    bookauthor.innerText = Library[index].author;
+    bookpages.innerText = `Pages: ${Library[index].pages}`;
+    bookread.innerText = Library[index].read;
+    if (Library[index].read) {
+        bookread.innerText = "Read";
+    } else {
+        bookread.innerText = "Not read"
+    }
+}
+addExampleBooks(Hobbit, 0);
+
+//remember when pressing close button for book, to remove it from the array, match the data-libraryindex="0"  number to the array
+//index to remove the right one from the array.
+
+/*testing clonenode before switching to this
+function  createBook() {
+let book = document.createElement("div");  //dont forget add data attribute later
+book.classList.add("book");
+book.setAttribute("data-libraryindex", `${Library.length - 1}`)
+
+let exit = document.createElement("div");
+exit.classList.add("exit");
+exit.innerText = "x";
+
+let title = document.createElement("div");
+title.classList.add("booktitle");
+title.innerText = `title placeholder`;
+
+let author = document.createElement("div");
+author.classList.add("bookauthor");
+author.innerText = `author placeholder`;
+
+let pages = document.createElement("div");
+pages.classList.add("bookpages");
+pages.innerText = `pages placeholder`;
+
+let read= document.createElement("div");
+read.classList.add("bookread"); 
+read.innerText = `read placeholder`;
 
 
-//if user doesn't have an image, have stock lieather book image that we use, so they select "no image"
+book.appendChild(exit);
+book.appendChild(title);
+book.appendChild(author);
+book.appendChild(pages);
+book.appendChild(read);
 
+maincontent.appendChild(book);
 
+}
+*/
 
-
-
-
+function createClone() {
+let clone = document.querySelector(".book").cloneNode("true");
+clone.setAttribute("data-libraryindex", `${Library.length + 1}`)
+maincontent.appendChild(clone);
+} 
 
 
 
