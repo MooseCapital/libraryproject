4fea0f57372a7305c154eed455c0e7d40ee75e8f
MooseCapital <48196495+MooseCapital@users.noreply.github.com>
2022-06-14 02:02:11 -0500

    exit button working on books
    Former-commit-id: 1c0a05b43051ff6236ac9a1d1afae2180a650e26

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let bookpages = document.querySelector(".bookpages");
 let bookread= document.querySelector(".bookread");
 const addButton = document.querySelector(".addbutton");
 let maincontent = document.querySelector(".maincontent");
-let book = document.querySelector(".book");
+
 const addto = document.querySelector(".addto");
-let exit = document.querySelectorAll(".exit");
+
 
 addto.addEventListener("click", openTheForm);
 addButton.addEventListener("click", createClone);
@@ -31,11 +31,7 @@ console.log(Hobbit.info())
 
 let Library = [];
 
-console.table(Library)
-function addBooktoLibrary() {
 
-
-}
 function addExampleBooks(bookobj, index) {
     Library.push(bookobj)
     booktitle.innerText = Library[index].title;
@@ -55,47 +51,71 @@ let bauthor = document.querySelector("#b-author");
 let bpages = document.querySelector("#b-pages");
 let clone;
 let bookvar;
-let currentdatalib;
-let libindex = 0;
+
 
 
 function createClone() {
-clone = document.querySelector(".book").cloneNode("true");
-
+    clone = document.querySelector(".book").cloneNode("true");
+    checkm.push(false);
 
 
 
 
 //when setting object name to title, remember to remove spaces
-bookvar = new Book(btitle.value.toString(),bauthor.value.toString(), bpages.value, false); //create new book object
+    bookvar = new Book(btitle.value.toString(),bauthor.value.toString(), bpages.value, false); //create new book object
 
-Library.push(bookvar) //adding the actual book to our array
+    Library.push(bookvar) //adding the actual book to our array
 
-clone.setAttribute("data-libraryindex", `${Library.length - 1}`);
+    clone.setAttribute("data-libraryindex", `${Library.length - 1}`);
 
-maincontent.appendChild(clone); //adding content to the actual page
-
-
-
-document.querySelector(`[data-libraryindex='${Library.length - 1}'] .booktitle `).innerText = btitle.value;
-document.querySelector(`[data-libraryindex='${Library.length - 1}'] .bookauthor `).innerText = bauthor.value;
-document.querySelector(`[data-libraryindex='${Library.length - 1}'] .bookpages `).innerText = `Pages: ${bpages.value}`;
+    maincontent.appendChild(clone); //adding content to the actual page
 
 
 
-btitle.value = "";
-bauthor.value = "";
-bpages.value = "";
+    document.querySelector(`[data-libraryindex='${Library.length - 1}'] .booktitle `).innerText = btitle.value;
+    document.querySelector(`[data-libraryindex='${Library.length - 1}'] .bookauthor `).innerText = bauthor.value;
+    document.querySelector(`[data-libraryindex='${Library.length - 1}'] .bookpages `).innerText = `Pages: ${bpages.value}`;
 
 
+
+    btitle.value = "";
+    bauthor.value = "";
+    bpages.value = "";
+
+    document.querySelectorAll(".book").forEach((item, index) => {
+        document.querySelectorAll(".exit").forEach((ex, index) => {
+            ex.addEventListener("click", (e) => {
+                e.target.parentElement.remove();
+            })
+        
+        })
+        document.querySelectorAll("#check").forEach( (check, index) => {
+            check.addEventListener("click", (e) => {
+                
+                if (checkm[index] === false) {
+
+                    checkm[index] = true;
+                    document.querySelector(`[data-libraryindex='${index}'] .bookread `).innerText = "Read";
+                    console.log("true");
+                } else if(checkm[index] === true){
+
+                    checkm[index] = false;
+                    document.querySelector(`[data-libraryindex='${index}'] .bookread `).innerText = "Not Read";
+                    console.log("false");
+                }
+            })
+        
+        }) 
+    })
+
+
+
+    console.table(Library);
+    
 } 
 
-// function setInnerText() {
-// booktitle.innerText = Library[Library.length - 1].title;
-// bookauthor.innerText = Library[Library.length - 1].author;
-// bookpages.innerText = `Pages: ${Library[Library.length - 1].pages}`;
-//
-// }
+
+
 
 function exitTheForm() {
 document.querySelector(".addbooksection").setAttribute("style", "display: none");
@@ -109,71 +129,26 @@ function openTheForm() {
 
 
 
-//remember when pressing close button for book, to remove it from the array, match the data-libraryindex="0"  number to the array
-//index to remove the right one from the array.
-
-/*testing clonenode before switching to this
-function  createBook() {
-let book = document.createElement("div");  //dont forget add data attribute later
-book.classList.add("book");
-book.setAttribute("data-libraryindex", `${Library.length - 1}`)
-
-let exit = document.createElement("div");
-exit.classList.add("exit");
-exit.innerText = "x";
-
-let title = document.createElement("div");
-title.classList.add("booktitle");
-title.innerText = `title placeholder`;
-
-let author = document.createElement("div");
-author.classList.add("bookauthor");
-author.innerText = `author placeholder`;
-
-let pages = document.createElement("div");
-pages.classList.add("bookpages");
-pages.innerText = `pages placeholder`;
-
-let read= document.createElement("div");
-read.classList.add("bookread"); 
-read.innerText = `read placeholder`;
+let checkm = [false];
 
 
-book.appendChild(exit);
-book.appendChild(title);
-book.appendChild(author);
-book.appendChild(pages);
-book.appendChild(read);
+document.querySelectorAll("#check").forEach( (check, index) => {
+    check.addEventListener("click", (e) => {
 
-maincontent.appendChild(book);
+        if (checkm[index] === false) {
 
-}
-*/
-let checkm = false;
-document.querySelector("#check").addEventListener("click", readCheck);
+            checkm[index] = true;
+            document.querySelector(`[data-libraryindex='${index}'] .bookread `).innerText = "Read";
+            
+        } else if(checkm[index] === true){
 
-function readCheck() {
-if (checkm == false) {
-    
-    checkm = true;
-    bookread.innerText = "Read";
-    console.log("true");
-} else if(checkm == true){
-    
-    checkm = false;
-    bookread.innerText = "Not Read";
-    console.log("false");
-}
+            checkm[index] = false;
+            document.querySelector(`[data-libraryindex='${index}'] .bookread `).innerText = "Not Read";
+            
+        }
+    })
 
-}
-
-exit.forEach(function (item) {
-    item.addEventListener("click", () => {
-       item.parentNode.remove();
-        })
-});
-
-
+})
 
 
 
