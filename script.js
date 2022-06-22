a5128b6674a7aeffbba1acf63209c276acac55ac
MooseCapital <48196495+MooseCapital@users.noreply.github.com>
2022-06-21 22:13:46 -0500

    exit button working on books

--- script.js
+++ script.js
@@ -12,12 +12,13 @@ addto.addEventListener("click", openTheForm);
 addButton.addEventListener("click", createClone);
 document.querySelector(".formexit").addEventListener("click", exitTheForm);
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 Book.prototype.info = function () {
